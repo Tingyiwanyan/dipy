d4561d827d78310cab80e3374005710b666be8a6
Tingyi Wanyan <tiwanyan@149-160-235-56.dhcp-bl.indiana.edu>
2016-11-03 14:25:33 -0400

    updata autobundle

--- doc/examples/first_experiment_autobundles.py
+++ doc/examples/first_experiment_autobundles.py
@@ -3,6 +3,8 @@ from dipy.viz import actor, window
 
 import numpy as np
 
+from dipy.exp_comparisons_experts import labelize_expert_bundles
+
 from dipy.align.streamlinear import remove_clusters_by_size
 
 from dipy.segment.clustering import QuickBundles
@@ -72,8 +74,9 @@ atlas_dix['uf.right'] = {'filename' : dname_atlas + 'bundles_uf.right.trk'}
 
 #dname_subj =
 
-streamlines_file = '/Users/tiwanyan/Mount/2013_02_13_Vincent_Laverdiere/streamlines_500K.trk'
+#streamlines_file = '/Users/tiwanyan/Mount/2013_02_13_Vincent_Laverdiere/streamlines_500K.trk'
 
+streamlines_file = '/Users/tiwanyan/Mount/2013_02_08_Gabriel_Girard/streamlines_500K.trk'
 
 from dipy.io.trackvis import load_trk
 
@@ -81,9 +84,15 @@ from nibabel.streamlines import ArraySequence
 
 full_atlas = ArraySequence()
 centroids_atlas = []
+centroids_atlas2 = []
+color_array = []
 
-metric = MinimumAverageDirectFlipMetric()
+#metric = MinimumAverageDirectFlipMetric()
 metric = AveragePointwiseEuclideanMetric()
+
+full_atlas2 = []
+keys = []
+
 
 for key in atlas_dix:
     filename = atlas_dix[key]['filename']
@@ -101,11 +110,32 @@ for key in atlas_dix:
 
     # show_streamlines(streamlines)
     full_atlas.extend(streamlines)
+    full_atlas2.append(streamlines)
+    keys.append(key)
     # show_streamlines(full_atlas)
     # del streamlines
 
+
+
+for key in atlas_dix:
+    filename = atlas_dix[key]['filename']
+
+    streamlines, header = load_trk(filename)
+    atlas_dix[key]['streamlines'] = streamlines
+    discrete_streamlines = set_number_of_points(streamlines, 20)
+    clusters = QuickBundles(threshold = 10.,
+                            metric=metric).cluster(discrete_streamlines)
+    atlas_dix[key]['centroids'] = clusters[0].centroid
+    centroids_atlas2 += [clusters[0].centroid]
+    centroids_atlas2 += [clusters[1].centroid]
+    centroids_atlas2 += [clusters[2].centroid]
+    centroids_atlas2 += [clusters[3].centroid]
+
 color_array = np.random.rand(len(centroids_atlas),3) * 10
+color_array2 = np.repeat(color_array,4,axis = 0)
+
 #show_streamlines(centroids_atlas,color_array)
+
 
 #streamlines_static, header_static = load_trk(dname_full_atlas_streamlines)
 streamlines_target, header_target = load_trk(streamlines_file)
@@ -114,70 +144,77 @@ print('Large streamlines loaded')
 
 #streamlines_target = set_number_of_points(streamlines, 20)
 
-discrete_target = set_number_of_points(streamlines_target, nb_points=20)
-
-distance_matrix = bundles_distances_mdf(centroids_atlas, discrete_target)
 
 #moved, matrix_affine, qb_centroids1, qb_centroids2 = whole_brain_slr(centroids_atlas, streamlines_target, x0='affine', rm_small_clusters=50, maxiter=100, select_random=None, verbose=False, greater_than=50, less_than=250, qb_thr=15, nb_pts=20, progressive=True)
 
-"""
-greater_than = 50
-less_than = 250
-qb_thr = 15
-nb_pts = 20
-rm_small_clusters=50
-progressive=True
-#x0='affine'
+def slr(streamlines_target,centroids_atlas2):
+    greater_than = 50
+    less_than = 250
+    qb_thr = 15
+    nb_pts = 20
+    rm_small_clusters=50
+    progressive=True
+    #x0='affine'
 
-def check_range(streamline, gt=greater_than, lt=less_than):
+    discrete_target = set_number_of_points(streamlines_target, nb_points=20)
 
-    if (len(streamline) > gt) & (len(streamline) < lt):
-        return True
-    else:
-        return False
+    def check_range(streamline, gt=greater_than, lt=less_than):
 
-streamlines1 = [s for s in streamlines_target if check_range(s)]
+        if (len(streamline) > gt) & (len(streamline) < lt):
+            return True
+        else:
+            return False
 
-rstreamlines1 = streamlines1
+    streamlines1 = [s for s in streamlines_target if check_range(s)]
 
-rstreamlines1 = set_number_of_points(rstreamlines1, nb_pts)
+    rstreamlines1 = streamlines1
 
-#print('Resample finished', sep=' ', end='n', file=sys.stdout, flush=False)
+    rstreamlines1 = set_number_of_points(rstreamlines1, nb_pts)
 
-qb1 = QuickBundles(threshold=qb_thr, metric=metric)
-rstreamlines1 = [s.astype('f4') for s in rstreamlines1]
-cluster_map1 = qb1.cluster(rstreamlines1)
-# clusters1 = remove_clusters_by_size(cluster_map1, rm_small_clusters)
-qb_centroids = [cluster.centroid for cluster in clusters1]
+    #print('Resample finished', sep=' ', end='n', file=sys.stdout, flush=False)
 
-#1/0
+    qb1 = QuickBundles(threshold=qb_thr, metric=metric)
+    rstreamlines1 = [s.astype('f4') for s in rstreamlines1]
+    cluster_map1 = qb1.cluster(rstreamlines1)
+    # clusters1 = remove_clusters_by_size(cluster_map1, rm_small_clusters)
+    qb_centroids = [cluster.centroid for cluster in cluster_map1]
 
-bounds = [(-45, 45), (-45, 45), (-45, 45),
-          (-30, 30), (-30, 30), (-30, 30),
-          (0.6, 1.4), (0.6, 1.4), (0.6, 1.4),
-          (-10, 10), (-10, 10), (-10, 10)]
-slm = progressive_slr(centroids_atlas, qb_centroids,
-                      x0='rigid', metric=None,
-                      bounds=bounds)
+    #1/0
+
+    bounds = [(-45, 45), (-45, 45), (-45, 45),
+              (-30, 30), (-30, 30), (-30, 30),
+              (0.6, 1.4), (0.6, 1.4), (0.6, 1.4),
+              (-10, 10), (-10, 10), (-10, 10)]
+    slm = progressive_slr(centroids_atlas2, qb_centroids,
+                          x0='rigid', metric=None,
+                          bounds=bounds)
 
 #show_streamlines(qb_centroids1, centroids_atlas,translate = True)
 #streamline_final = ArraySequence()
 #color_array_target = []
-"""
+    return slm.transform(discrete_target)
+
+discrete_target = slr(streamlines_target,centroids_atlas2)
+
+distance_matrix = bundles_distances_mdf(centroids_atlas2, discrete_target)
 
 index_whole = np.argmin(distance_matrix,axis=0)
 
 stream_line_min = np.amin(distance_matrix,axis=0)
 
-index_threshold = np.where(stream_line_min < 20)
+index_threshold = np.where(stream_line_min < 10)
 
 streamline_final = streamlines_target[index_threshold[0]]
 
-color_array_target = color_array[index_whole[index_threshold[0]],:]
+color_array_target = color_array2[index_whole[index_threshold[0]],:]
 
 
+#bundle_labels_npy = []
 
-show_streamlines(centroids_atlas,streamline_final,color_array,color_array_target,True)
+#bundle_labels_npy, bundle_labels = labelize_expert_bundles(streamlines_target, full_atlas2[0], bundle_labels_npy)
 
+
+show_streamlines(centroids_atlas2,streamline_final,color_array2,color_array_target,True)
+#show_streamlines(centroids_atlas,centroids_atlas2,color_array,color_array2,True)
 #for i in index_threshold[0]:
 #    streamline_final.extend(streamlines_target[i])
